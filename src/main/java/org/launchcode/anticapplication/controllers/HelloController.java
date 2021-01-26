c3d7d6770512247a37e1b81841d954932c29099e
TheAnticSits <kenmessmerjr@gmail.com>
2021-01-25 20:52:11 -0600

    set up a dynamic response with variable in the parameters

--- src/main/java/org/launchcode/anticapplication/controllers/HelloController.java
+++ src/main/java/org/launchcode/anticapplication/controllers/HelloController.java
@@ -2,17 +2,19 @@ package org.launchcode.anticapplication.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+/*
     //handles requests at /hello
+    //static response
     @GetMapping("hello")
     @ResponseBody
     public String hello(){
         return "Hello Antic";
-    }
+    }*/
 
     //handles requests at /goodbye
     @GetMapping("goodbye")
@@ -20,4 +22,15 @@ public class HelloController {
     public String goodbye(){
         return "Goodbye Antic";
     }
+
+    //Dynamic Response
+    //Handles request of the form /hello?name=Launchcode
+    //lives at http://localhost:8080/hello?name=Antic
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
 }
