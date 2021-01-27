b3c5b1bb476a54aabcc9496242b9ecdc8497d436
TheAnticSits <kenmessmerjr@gmail.com>
2021-01-26 23:25:57 -0600

    Created template that contains a loop that allows to display items passed in in list.  Created a names list.

--- src/main/java/org/launchcode/anticapplication/controllers/HelloController.java
+++ src/main/java/org/launchcode/anticapplication/controllers/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -16,14 +19,30 @@ public class HelloController {
 
     //handles requests of the form /hello/LaunchCode
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm(){
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Jim");
+        names.add("Pam");
+        names.add("Michael");
+        names.add("Dwight");
+        names.add("Oscar");
+        names.add("Kevin");
+        names.add("Stanley");
+        names.add("Angela");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
