a63651dc1593a43abd3c592c92cd8739f4fc2aed
TheAnticSits <kenmessmerjr@gmail.com>
2021-01-26 22:58:33 -0600

    Cleaned up code a bit

--- src/main/java/org/launchcode/anticapplication/controllers/HelloController.java
+++ src/main/java/org/launchcode/anticapplication/controllers/HelloController.java
@@ -3,53 +3,20 @@ package org.launchcode.anticapplication.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller//@RequestMapping("hello")
+@Controller
 public class HelloController {
-/*
-    //handles requests at /hello
-    //static response
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello Antic";
-    }*/
-
-    //handles requests at /hello/goodbye
-    @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye(){
-        return "Goodbye Antic";
-    }
-
-    //Dynamic Response
-    //Handles request of the form /hello?name=Launchcode
-    //lives at http://localhost:8080/hello?name=Antic
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
     //handles requests of the form /hello/LaunchCode
-    @GetMapping("/{name}")
+    @GetMapping("/hello/{name}")
     @ResponseBody
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
-
-/*    //lives at /hello/form
-    @GetMapping("form")
-    public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
-    }*/
 
     @GetMapping("form")
     public String helloForm(){
