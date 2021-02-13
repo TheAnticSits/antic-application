df0d0da587a1e4af04f6090aaf0c8e103cb9024f
TheAnticSits <kenmessmerjr@gmail.com>
2021-02-12 23:34:52 -0600

    Added data description to model class

--- src/main/java/org/launchcode/anticapplication/controllers/EventsController.java
+++ src/main/java/org/launchcode/anticapplication/controllers/EventsController.java
@@ -31,8 +31,8 @@ public class EventsController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(new Event(eventName));
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
+        events.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
